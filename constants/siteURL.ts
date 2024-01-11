415ac7892a9e010707acf169d750ae2d2a8084ca
marukun712 <marukun530@gmail.com>
2024-01-11 16:07:54 +0900

    🐛CallbackURLの変更

--- constants/siteURL.ts
+++ constants/siteURL.ts
@@ -1,3 +1,3 @@
 export const siteURL = process.env.NEXT_PUBLIC_VERCEL_URL
-    ? 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL
+    ? 'https://vrmeet-collab.vercel.app'
     : 'http://localhost:3000'
