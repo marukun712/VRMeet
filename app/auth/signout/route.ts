d373dc1214d38d0460f40d0b950cce178c0e3ad3
marukun712 <marukun530@gmail.com>
2024-10-14 23:04:04 +0900

    🎨 フォーマッタを変更2

--- app/auth/signout/route.ts
+++ app/auth/signout/route.ts
@@ -1,20 +1,20 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import { NextRequest, NextResponse } from 'next/server'
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-    const supabase = createRouteHandlerClient({ cookies })
+  const supabase = createRouteHandlerClient({ cookies });
 
-    //セッションの存在をチェック
-    const {
-        data: { session },
-    } = await supabase.auth.getSession()
+  //セッションの存在をチェック
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
-    if (session) {
-        await supabase.auth.signOut() //セッションが存在すればサインアウト
-    }
+  if (session) {
+    await supabase.auth.signOut(); //セッションが存在すればサインアウト
+  }
 
-    return NextResponse.redirect(new URL('/', req.url), {
-        status: 302,
-    })
+  return NextResponse.redirect(new URL("/", req.url), {
+    status: 302,
+  });
 }
